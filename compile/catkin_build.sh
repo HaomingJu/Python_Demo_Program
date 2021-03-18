e603f38ac5b5bb573a411ed1af42e01cb698123a
haomingju <juhaoming@126.com>
2021-03-18 18:27:37 +0800

    设置优先级以及最大线程数目

--- compile/catkin_build.sh
+++ compile/catkin_build.sh
@@ -5,11 +5,15 @@
 # 配置文件 /etc/default/distcc
 # 参考: https://github.com/kuka-isir/rtt_lwr/blob/rtt_lwr-2.0/docs/source/adv-tutos/ccache-distcc.rst
 
+# export DISTCC_HOSTS="192.168.3.92/16 localhost/8"
+# 设置服务器优先级以及最大核心数目
+
 if [[ $1 ]]; then
     cd $1 && \
         echo `pwd`  && \
-        export CCACHE_PREFIX="distcc" && \
+        export CCACHE_PREFIX="distcc"
         export CC="ccache gcc" CXX="ccache g++"
+        export DISTCC_HOSTS="192.168.3.92/16 localhost/8"
 
         catkin config --install && \
         catkin build  -p32 -j32 --no-jobserver --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && \
